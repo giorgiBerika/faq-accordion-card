f3fd61abf44726faa212f0cd76d561d400c5aa83
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-08-22 10:06:24 +0400

    Adding images to left part of App component

--- src/app-parts/image-part/AppLeft.jsx
+++ src/app-parts/image-part/AppLeft.jsx
@@ -1,9 +1,17 @@
 import './AppLeft.css'
 
+import platformLines from '../../assets/lines-platform.png';
+import mainImage from '../../assets/main-image.png';
+import mailBox from '../../assets/mail-box.svg'
+
 const AppLeft = () =>
 {
     return (
-        <div>Left Part!</div>
+        <div className='app-left-part'>
+            <img className='platform-lines' src={platformLines} alt="Platform and lines" />
+            <img className='mail-box' src={mailBox} alt="mail 3d box" />
+            <img src={mainImage} alt="A girl and pc, 3d art" />
+        </div>
     )
 }
 
