8135b23ebdfefb218c69334f766a89c68c677772
giorgiBerika <giorgi.berikashvili.3@iliauni.edu.ge>
2023-08-23 09:11:15 +0400

    Adding responsive to the page

--- src/app-parts/image-part/AppLeft.jsx
+++ src/app-parts/image-part/AppLeft.jsx
@@ -3,6 +3,7 @@ import './AppLeft.css'
 import platformLines from '../../assets/lines-platform.png';
 import mainImage from '../../assets/main-image.png';
 import mailBox from '../../assets/mail-box.svg'
+import singlePlatform from '../../assets/single-platform.svg'
 
 const AppLeft = () =>
 {
@@ -11,6 +12,7 @@ const AppLeft = () =>
             <img className='platform-lines' src={platformLines} alt="Platform and lines" />
             <img className='mail-box' src={mailBox} alt="mail 3d box" />
             <img src={mainImage} alt="A girl and pc, 3d art" />
+            <img className='single-platform' src={singlePlatform} alt='Just platform'/>
         </div>
     )
 }
